d49fcdecb1ff4c190810e8b03e850849d05f4ae6
Max Pronko <max.pronko@gmail.com>
2019-10-07 08:07:13 +0100

    Updates

---
+++ component---src-pages-page-2-js-9e29505b2dbef9e479fc.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{213:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(85),c=a(210),r=a(209);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-9e29505b2dbef9e479fc.js.map
